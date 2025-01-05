21db08cf92072175fbd109476003faa0cbade536
faroukm2002 <faroukm238@gmail.com>
2025-01-06 00:32:53 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { dbConnection } from './../database/dbConnection';
+import express  from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import morgan from "morgan";
+import { bootstrap } from './bootstrap';
+dotenv.config()
+
+const app = express();
+
+// Middleware for JSON body parsing
+app.use(express.json({limit: "10kb"}));
+app.use(express.urlencoded({extended: false}));
+
+
+// Logger for development
+if (process.env.NODE_ENV === 'development') { 
+    app.use(morgan("dev"))
+  
+}
+
+
+
+const port :number  = parseInt(process.env.PORT as string) || 10000
+
+
+dbConnection()
+bootstrap(app);
+
+
+
+app.listen(port, ():void =>
+    console.log(`Example app listening on port http://localhost:${port}`)) 
+ 
